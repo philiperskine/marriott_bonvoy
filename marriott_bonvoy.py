6259f313d64cb9adcb994475dd2acba1d372ba01
Philip Erskine <164562064+philiperskine@users.noreply.github.com>
2026-01-08 14:46:31 +0000

    New dictionary

--- marriott_bonvoy.py
+++ marriott_bonvoy.py
@@ -1,15 +1,21 @@
 import random
 
-# MEMBER CODES
+# MEMBER LEVEL DICTIONARIES
 # No code! -> Non-member
-# Y1 -> Member           (0-9 Nights/Year)
-# M1 -> Silver Elite     (10-24 Nights/Year)
-# X1 -> Gold Elite       (25-49 Nights/Year)
-# P6 -> Platinum Elite   (50-74 Nights/Year)
-# X4 -> Titanium Elite   (75+ Nights/Year)
-# X5 -> Ambassador Elite (100+ Nights and 23k USD qualifying spend/Year)
+# Y1 Member           (0-9 Nights/Year)
+member = {
+    "code": "Y1",
+    "min_nights": "0",
+    "max_nights": "9"
+}
 
-# MEMBER LEVELS
+# M1 Silver Elite     (10-24 Nights/Year)
+# X1 Gold Elite       (25-49 Nights/Year)
+# P6 Platinum Elite   (50-74 Nights/Year)
+# X4 Titanium Elite   (75+ Nights/Year)
+# X5 Ambassador Elite (100+ Nights and 23k USD qualifying spend/Year)
+
+# MEMBER LEVEL LIST
 member_level_list = ['Non-member', 'Member', 'Silver Elite', 'Gold Elite', 'Platinum Elite', 'Titanium Elite', 'Ambassador Elite']
 # Random member level
 random_member_level = (member_level_list[random.randint(0,6)])
@@ -24,7 +30,6 @@ if random_member_level == member_level_list[0]:
         print("CORRECT")
     else:
         print("INCORRECT")
-print("There is no code for a non-member.")
 
 # BENEFITS
 # Member
