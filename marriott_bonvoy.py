5b68d6904a53a6741eac28c48e92cbe9186076aa
Philip Erskine <164562064+philiperskine@users.noreply.github.com>
2026-02-01 13:54:08 +0000

    Simplified question structure

--- marriott_bonvoy.py
+++ marriott_bonvoy.py
@@ -1,35 +1,63 @@
 import random
 
+""" Assign a question to an answer as a key value pair
+Every time a question is generated
+eg. question 1:"""
+
+score = 0
+question = ''
+answer = ''
+correct = False
+
+# Main
+if correct = False:
+    input("What is the answer to this question?: ")
+        # Correct answer
+        if answer == input().lower
+            print ("Correct! Well done. + 1 point")
+            score += 1
+            correct = True
+        # Incorrect answer     
+        else:
+            print("Incorrect. Try again. - 1 point")
+            score -= 1
+
 # MEMBER LEVEL DICTIONARIES
 # No code! -> Non-member
 # Y1 Member           (0-9 Nights/Year)
-member = {
-    "code": "Y1",
-    "min_nights": "0",
-    "max_nights": "9"
-}
+# member = {
+    # "code": "Y1",
+    # "min_nights": "0",
+    # "max_nights": "9"
+# }
 
+# Y1 Member           (0-9 Nights/Year)
 # M1 Silver Elite     (10-24 Nights/Year)
 # X1 Gold Elite       (25-49 Nights/Year)
 # P6 Platinum Elite   (50-74 Nights/Year)
 # X4 Titanium Elite   (75+ Nights/Year)
 # X5 Ambassador Elite (100+ Nights and 23k USD qualifying spend/Year)
 
-# MEMBER LEVEL LIST
-member_level_list = ['Non-member', 'Member', 'Silver Elite', 'Gold Elite', 'Platinum Elite', 'Titanium Elite', 'Ambassador Elite']
-# Random member level
-random_member_level = (member_level_list[random.randint(0,6)])
-
 # MAIN
-# Question for all levels with a code (i.e. not Non-member)
-user_input = input("What is the member code for " + random_member_level + " level?: ")
-
-# Index 1 -> Member
-if random_member_level == member_level_list[0]:
-    if user_input == "none":
-        print("CORRECT")
+while True:
+    # Member level list
+    member_level_list = ['Non-member', 'Member', 'Silver Elite', 'Gold Elite', 'Platinum Elite', 'Titanium Elite', 'Ambassador Elite']
+    # Random member level
+    random_member_level = (member_level_list[random.randint(0,6)])
+    # Member code question
+    if random_member_level == member_level_list[0]:
+        user_input = input("Does a non-member have a member code? ")
     else:
-        print("INCORRECT")
+        user_input = input("What is the member code for " + random_member_level + " level?: ")
+
+# Index 0 -> Non-member
+    while random_member_level == member_level_list[0]:
+        if user_input == "no":
+            print("CORRECT")
+            break
+        else:
+            print("INCORRECT")
+            break
 
 # BENEFITS
 # Member
