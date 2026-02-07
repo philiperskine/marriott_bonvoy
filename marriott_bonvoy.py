401efdb0bd17cb20a0b9e0ba53ea2c27d17f015e
Philip Erskine <164562064+philiperskine@users.noreply.github.com>
2026-02-07 10:24:38 +0000

    More questions but need fix because random question keeps repeating

--- marriott_bonvoy.py
+++ marriott_bonvoy.py
@@ -1,19 +1,24 @@
 import random
 import time
 
-""" Make a dictionary of questions
-Assign a question to an answer as a key value pair
-Every time a question is generated
-eg. question 1:"""
-
 # QUESTION DICTIONARY
 question_list = {
-    '1':"What is the name of the hotel? "
+    '1':"What is the member code for a Member (0-9 Nights/Year)? ",
+    '2':"What is the member code for a Silver Elite member (10-24 Nights/Year)? ",
+    '3':"What is the member code for a Gold Elite member (25-49 Nights/Year)? ",
+    '4':"What is the member code for a Platinum Elite member (50-74 Nights/Year)? ",
+    '5':"What is the member code for a Titanium Elite member (75+ Nights/Year)? ",
+    '6':"What is the member code for a Ambassador Elite member (100+ Nights and 23k USD qualifying spend/Year)? "
 }
 
 # ANSWER DICTIONARY
 answer_list = {
-    '1':"Bankside"
+    '1':"Y1",
+    '2':"M1",
+    '3':"X1",
+    '4':"P6",
+    '5':"X4",
+    '6':"X5"
 }
 
 # MAIN
@@ -22,12 +27,12 @@ how_many_questions = 3
 current_question_number = 0
 score = 0
 # Random question key from question_list dictionary 
-current_random_question_key = str(random.randint(1,len(question_list)))
-current_question = (question_list[current_random_question_key])
-current_correct_answer = (answer_list[current_random_question_key])
+current_random_key = str(random.randint(1,len(question_list)))
+current_question = (question_list[current_random_key])
+current_correct_answer = (answer_list[current_random_key])
 
 print("Welcome to the Marriott Bonvoy quiz!")
-time.sleep(2)
+time.sleep(1)
 print("Here are " + str(how_many_questions) + " questions to test your Marriott Bonvoy knowledge.")
 time.sleep(1)
 while current_question_number < how_many_questions:
@@ -35,11 +40,10 @@ while current_question_number < how_many_questions:
     # Correct answer
     if current_correct_answer.lower() == user_answer.lower():
         score += 1
-        print("Correct")
+        print("Correct! + 1 point!")
     # Incorrect answer     
     else:
         print("Incorrect")
-    print("Your score is now: " + str(score))
     current_question_number += 1
     time.sleep(1)
 
@@ -55,13 +59,6 @@ print("Your final score is: " + str(score))
     # "max_nights": "9"
 # }
 
-# Y1 Member           (0-9 Nights/Year)
-# M1 Silver Elite     (10-24 Nights/Year)
-# X1 Gold Elite       (25-49 Nights/Year)
-# P6 Platinum Elite   (50-74 Nights/Year)
-# X4 Titanium Elite   (75+ Nights/Year)
-# X5 Ambassador Elite (100+ Nights and 23k USD qualifying spend/Year)
-
 # MAIN
 # while True:
     # Member level list
@@ -74,42 +71,12 @@ print("Your final score is: " + str(score))
     # else:
         # user_input = input("What is the member code for " + random_member_level + " level?: ")
 
-# Index 0 -> Non-member
-    # while random_member_level == member_level_list[0]:
-        # if user_input == "no":
-            # print("CORRECT")
-            # break
-        # else:
-            # print("INCORRECT")
-            # break
-
 # BENEFITS
 # Member
 # member = ["Mobile App Services", "Member-only Rates", "Free Wi-Fi"]
 # member_length = len(member)
 
-
 # Silver Elite
 # silver_extras = ["Elite Reservation/Guest Service Line", "10% Gift Shop Discount", "Ultimate Reservation Guarantee", "10% Bonus on Loyalty Base Points", "Priority Late Check Out"]
 # silver_elite = member + silver_extras
 # silver_length = len(silver_elite)
-
-# Gold Elite
-# Platinum Elite
-# Titanium Elite
-# Ambassador Elite
-
-# For selecting a random membership type to be quizzed on
-# random_membership = random.randint(1,6)
-# print(random_membership)
-#1: member
-#2: silver_elite
-#3: gold_elite
-#4: platinum_elite
-#5: titanium_elite
-#6: ambassador_elite
-
-# i = 0
-# while i < member_length:
-    # print(member[i])
-    # i += 1
